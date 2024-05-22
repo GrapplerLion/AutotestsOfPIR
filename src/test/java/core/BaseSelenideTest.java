8adf3df9ad245d2dc3aa7ba928c95b831b4a71ed
r.galeev <r.galeev@csbi.ru>
2024-05-22 18:54:38 +0300

    add changes headless

--- src/test/java/core/BaseSelenideTest.java
+++ src/test/java/core/BaseSelenideTest.java
@@ -6,6 +6,7 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Базовый класс для инициализации селенида
@@ -28,6 +29,19 @@ abstract public class BaseSelenideTest extends Driver{
         Configuration.browserVersion = "119.0.6045.124";
         Configuration.browserSize = "2560x1440";
         Configuration.headless = true;
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=2560,1440");
+
+        Configuration.browserCapabilities = options;
+
+        System.out.println("Chrome version: " + Configuration.browserVersion);
+        System.out.println("Headless mode: " + Configuration.headless);
+        System.out.println("Browser size: " + Configuration.browserSize);
     }
 
     /**
