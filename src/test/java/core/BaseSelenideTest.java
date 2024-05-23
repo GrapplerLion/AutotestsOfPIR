3d09a5c5b0a0629808d281ad274b825abaf288cd
r.galeev <r.galeev@csbi.ru>
2024-05-23 16:17:41 +0300

    add changes settings update chrome

--- src/test/java/core/BaseSelenideTest.java
+++ src/test/java/core/BaseSelenideTest.java
@@ -22,13 +22,13 @@ abstract public class BaseSelenideTest extends Driver{
      * Инициализация selenide с настройками
      */
     public void setUp(){
-        WebDriverManager.chromedriver().driverVersion("125.0.6422.76").setup();
+        WebDriverManager.chromedriver().driverVersion("124.0.6367.78").setup();
 
 //        WebDriverManager.chromedriver().setup();
         Configuration.browser = "" +
                 "chrome";
         Configuration.webdriverLogsEnabled = true;
-        Configuration.browserVersion = "125.0.6422.76";
+        Configuration.browserVersion = "124.0.6367.78";
         Configuration.browserSize = "2560x1440";
         Configuration.headless = true;
 
