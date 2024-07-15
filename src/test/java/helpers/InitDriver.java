2be7828d74d1aa2d76571781cafb04f79761c081
Казаев Азат <a.kazaev@csbi.ru>
2024-07-15 16:40:56 +0300

    Update src/test/java/helpers/InitDriver.java

--- src/test/java/helpers/InitDriver.java
+++ src/test/java/helpers/InitDriver.java
@@ -23,7 +23,7 @@ abstract public class InitDriver {
         switch (TestConfig.browser.toLowerCase()) {
             case "chrome":
                 // System.setProperty("webdriver.chrome.driver", "drivers/chromedriver-win64/chromedriver.exe");
-                System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver")
+                System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 break;
